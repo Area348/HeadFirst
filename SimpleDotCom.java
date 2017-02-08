83f5067794aed0743d115718ecc871dc3ceb991f
Area348 <github@area348.nl>
2017-02-08 08:32:15 +0100

    GameHelper

--- SimpleDotCom.java
+++ SimpleDotCom.java
@@ -31,49 +31,31 @@ public class SimpleDotCom
 		System.out.println(result);
 		return result;
 	} // close method
-} // close class
-/*
-public class SimpleDotCom
-{
-//-- Set vars.
-	int[] locationCells;
-	int numOfHits = 0;
-
-	public void setLocationCells(int[] locs)
+	
+	public static void main(String[] args)
 	{
-		locationCells = locs;
-	}
-	
-	
-	
-//-- Method om te kijken of er een hit, miss of kill is.
-	public String checkYourself(String stringGuess)
-	{
-		int guess 		= Integer.parseInt(stringGuess);
-		String result 	= "miss";
+		int 		numOfGuesses 	= 0;
+		GameHelper 	helper 			= new GameHelper();
 		
-	//-- Loop de inhoud van locationCells net zo vaak als dat dat die inhout heeft.
-		for (int cell : locationCells)
+		SimpleDotCom theDotCom 		= new SimpleDotCom();
+		int 		randomNum 		= (int) (Math.random()*5);
+		int[] 		locations 		= {randomNum, randomNum+1, randomNum+2};
+		theDotCom.setLocationCells(locations);
+		boolean 	isAlive = true;
+		
+		while(isAlive == true)
 		{
-		//-- Als de ingevoerd waarde gelijk is aan een cell met target geef een hit als resultaat.
-			if(guess == cell)
+			String guess 	= helper.getUserInput("enter a number");
+			String result 	= theDotCom.checkYourself(guess);
+			
+			numOfGuesses++;
+			
+			if(result.equals("kill"))
 			{
-				result = "hit";		//-- Set de waarde hit in result.
-				numOfHits++;		//-- doe +1 bij numOfHits
-				break;				//-- Stop de for loop omdat er al een hit is.
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " guesses");
 			}
 		}
-		
-	//-- Kijk of er net zo veel hits zijn als dat locationCells lang is.	
-		if (numOfHits == locationCells.length)
-		{
-		//-- Set de waarde kill in result.
-			result = "kill";
-		}
-		
-		System.out.println(result);
-		return result;	//-- Geef de waarde in result terug naar waar de method aan gesproken wordt.
-		
 	}
-}
-*/
+	
+} // close class
