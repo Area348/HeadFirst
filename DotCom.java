739c4194955fca2417d06afd2387f215855e2c83
Area348 <github@area348.nl>
2017-02-10 14:33:56 +0100

    DotComBust

--- DotCom.java
+++ DotCom.java
@@ -30,34 +30,5 @@ public class DotCom
 		}//Close outer if
 		
 		return result;
-	} // close method
-	
-	public static void main(String[] args)
-	{
-		int 		numOfGuesses 	= 0;
-		GameHelper 	helper 			= new GameHelper();
-		
-		SimpleDotCom theDotCom 		= new SimpleDotCom();
-		int 		randomNum 		= (int) (Math.random()*5);
-		int[] 		locations 		= {randomNum, randomNum+1, randomNum+2};
-		theDotCom.setLocationCells(locations);
-		boolean 	isAlive = true;
-		
-		while(isAlive == true)
-		{
-			String guess 	= helper.getUserInput("enter a number");
-			String result 	= theDotCom.checkYourself(guess);
-			
-			numOfGuesses++;
-			
-			System.out.println(result);
-			
-			if(result.equals("kill"))
-			{
-				isAlive = false;
-				System.out.println("You took " + numOfGuesses + " guesses");
-			}
-		}
-	}
-	
+	} // close method	
 } // close class
---
+++ DotComBust.java
@@ -0,0 +1,13 @@
+//HeadFirst 2e.
+//P 146
+
+import java.util.*;
+
+public class DotComBust
+{
+	private GameHelper helper 				= new GameHelper();
+	private ArrayList<DotCom> dorComsList	= new ArrayList<DotCom>;	
+	private int numOfGuesses				= 0;
+	
+	
+}
