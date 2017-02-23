4678fbddb6e620605374c12bd5436b55c89a09c3
Area348 <github@area348.nl>
2017-02-23 21:13:34 +0100

    GSON test
    test.java gemaakt om de GSON te testen.

---
+++ test.java
@@ -0,0 +1,22 @@
+//-- javac -cp ".;g:\git\HeadFirst\gson-2.6.2.jar" test.java
+
+import com.google.gson.Gson;
+
+public class test
+{
+	
+	public static void main(String args[])
+	{
+		
+		System.out.print("test 123");
+		
+		
+		Gson gson = new Gson();
+		
+		
+		System.out.println(" 456");
+	}
+	
+	
+	
+}
